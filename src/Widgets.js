55ac4a13b23ccf3d6b10328046671593dd347120
paik <praveenpaikadan2@gmail.com>
2020-08-13 00:12:15 +0530

    [Widgets done!]

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Widgets = () => {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widget__inputContainer">
+        <div className="widgets__input">
+          <SearchIcon className="widget__searchIcon" />
+          <input placeholder="Search Twitter" type="text" />
+        </div>
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1293335144775004160"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="realDonaldTrump"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"http://google.com"}
+          options={{ text: "#react js", via: "realdonaldtrump" }}
+        />
+        {/*The abouve component will lead to twitter tweet page with content*/}
+      </div>
     </div>
   );
 };
